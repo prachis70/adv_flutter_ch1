33c8f1d97431e4027ac368c14b2d266bb6de6892
Tc Family <singhprachi9496@gmail.com>
2024-07-07 14:57:49 +0530

    prachi

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,8 @@ class _themsDataState extends State<themsData> {
           body: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Stack( children: [
-                Container(height: height * 0.959, width: width * double.infinity,),
-                Container(height: height * 0.91, width: width * 0.9,decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),boxShadow:[BoxShadow(color: Colors.black12, spreadRadius: 1, blurRadius: 1)], color: Colors.black54),
+                Container(height: height * 0.959, width: width * double.infinity, decoration: BoxDecoration(color: Colors.white54),),
+                Container(height: height * 0.91, width: width * 0.9,decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),boxShadow:[BoxShadow(color: Colors.black12, spreadRadius: 1, blurRadius: 1)], color: Colors.white),
                   child:Padding(
                     padding: const EdgeInsets.all(110.0),
                     child:RichText(text: TextSpan(children: [TextSpan(text: '\nYo Man !\n ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.black),),TextSpan(text:'\nits a simple example of  \ndark Theme ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.black54),children: []), ],),),
@@ -81,4 +81,5 @@ class _themsDataState extends State<themsData> {
     );
   }
 }
+
 bool isDark = false;
